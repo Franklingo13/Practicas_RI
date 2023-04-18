f905a15f10e5346712fbfd4726425067ee41f28c
Franklin Gómez <franklingo13@gmail.com>
2023-04-17 19:07:34 -0500

    Update conf_inicial.ino
    Reproduce canciones mediante el DAC interno del ESP32
    Identifica atributos y los imprime por el monitor serial

--- Trabajo Corto C4/Arduino/conf_inicial/conf_inicial.ino
+++ Trabajo Corto C4/Arduino/conf_inicial/conf_inicial.ino
@@ -1,12 +1,50 @@
 #include "BluetoothA2DPSink.h"
 
 BluetoothA2DPSink a2dp_sink;
+bool is_active = true;
+String titulo;
+String estado;
+String t_play;
+void avrc_metadata_callback(uint8_t data1, const uint8_t *data2) {
+  
+  Serial.printf("AVRC metadata rsp: attribute id 0x%x, %s\n", data1, data2);
+    //convertir a string *data2
+  String atributo = String((char*)data2);
+  //Serial.println(s);
+  //Serial.println(s.length());
+  //Serial.println(s.substring(0, s.length()-1));
+
+  //condicionales para identificar el atributo
+    if (data1 == 0x01){
+      Serial.println("Titulo: ");
+      titulo = atributo;
+      Serial.println(titulo);
+    } else if (data1 == 0x02){
+      Serial.println("Artista");
+    } else if (data1 == 0x03){
+      Serial.println("Album");
+    } else if (data1 == 0x04){
+      Serial.println("Género");
+    } else if (data1 == 0x05){
+      Serial.println("Tiempo de reproducción: ");
+      t_play = atributo;
+      Serial.println(t_play);
+    }
+//  Serial.printf("Reproduciendo: ");
+//  Serial.println(data1);
+    // condicionales para identificar el estado
+    if (a2dp_sink.get_audio_state()==ESP_A2D_AUDIO_STATE_STARTED){
+      //Serial.println("play");
+      estado = "Play";
+      //a2dp_sink.play();
+    } else {
+      //Serial.println("pause");
+      estado = "Pause";
+    }
+}
 
 void setup() {
   Serial.begin(115200);
-  // intento de poder leer el nombre de la canción
-//  a2dp_sink.set_on_data_received(data_received_callback);
-  a2dp_sink.set_stream_reader(read_data_stream);
   //A2DS Sink empleando el DAC interno
   static const i2s_config_t i2s_config = {
         .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
@@ -19,15 +57,15 @@ void setup() {
         .dma_buf_len = 64,
         .use_apll = false
     };
-
+    //set_avrc_metadata_attribute_mask(ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_PLAYING_TIME);
     a2dp_sink.set_i2s_config(i2s_config);
     a2dp_sink.start("MyMusic");
+    
+    a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
+    //a2dp_sink.set_avrc_metadata_attribute_mask(ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_PLAYING_TIME);
 
 }
 
-//void data_received_callback() {
-//  Serial.println("Data packet received");
-//}
 
 void read_data_stream(const uint8_t *data, uint32_t length)
 {
@@ -36,7 +74,11 @@ void read_data_stream(const uint8_t *data, uint32_t length)
   Serial.println(*samples);
   // Do something with the data packet
 }
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+ 
 
 }
